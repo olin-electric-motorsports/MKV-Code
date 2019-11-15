0ad550fe4567c60cde7c1100a3367e8321ec2674
Wesley Soo-Hoo <wesoohoo@gmail.com>
2019-11-14 22:29:05 -0500

    Implement speed calculations

--- boards/wheelspeed/wheelspeed.c
+++ boards/wheelspeed/wheelspeed.c
@@ -6,8 +6,10 @@
 #include "log_uart.h"
 
 #define TIME_COUNT_MS 100
+#define HOLES_PER_REV 1
 
 uint16_t current_count = 0;
+float rpm = 0;
 char uart_report_msg[6*20+4+5] = "";
 
 ISR(PCINT0_vect) {
@@ -16,6 +18,16 @@ ISR(PCINT0_vect) {
     // TODO: Only flip LED when PB4 is triggered, not anything
     // on PCINT0
     PORTB ^= _BV(PB1);
+    current_count += 1;
+}
+
+uint16_t get_speed(uint16_t counts) {
+    /**
+     * Calculates RPS from the counts in the last timestep
+     *
+     * Returns: RPS * 1000
+     */
+    return (uint16_t) ((double) counts / (double) HOLES_PER_REV) / ((double) TIME_COUNT_MS / 1000.0) * 1000;
 }
 
 int main(void) {
@@ -32,12 +44,13 @@ int main(void) {
 
     while(1) {
         // Report data over UART
-        sprintf(uart_report_msg, "%i", current_count);
+        sprintf(uart_report_msg, "%u count, %u rpm", current_count, get_speed(current_count));
         LOG_println(uart_report_msg, strlen(uart_report_msg));
-        _delay_ms(TIME_COUNT_MS);
 
-        // Every (insert time step here) clear the current count
+        // Every time step clear the current count
         current_count = 0;
+
+        _delay_ms(TIME_COUNT_MS);
     }
 }
 
