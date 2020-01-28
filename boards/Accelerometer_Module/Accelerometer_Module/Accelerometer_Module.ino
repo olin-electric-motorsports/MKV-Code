e2bf6ee075cfd5dcbe399636ca4f1c78cb094078
nonas-hunter <lukenonashunter@gmail.com>
2020-01-27 21:00:00 -0500

    Establishing Outline

---
+++ boards/Accelerometer_Module/Accelerometer_Module/Accelerometer_Module.ino
@@ -0,0 +1,29 @@
+/*
+ * Board: Accelerometer Module
+ * Version: v1.0
+ * Description: The goal of this board is to measure the frequency of vibrations in the chassis
+ * in order to complete modal analysis.
+ */
+
+// TODO: Code everything.
+  // Turn on board & set everything up
+  // Initialize Accelerometer
+  // Wait for button press
+  // Record data
+  // Wait for button press
+  // Stop recording data
+  // Send data to SD card
+
+// Variable Definition
+
+void setup() {
+  // Initalization
+  
+
+}
+
+void loop() {
+  // Main Loop (button press, record, button press, send)
+
+  
+}
