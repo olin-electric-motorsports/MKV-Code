6b3ca6dbe63a694057426b52e33fbc5597ba2e63
Duncan Mazza <dmazza@olin.edu>
2020-01-30 21:19:08 -0500

    Generalize pip command

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ echo "Installing libraries"
 sudo apt-get update
 sudo apt-get install gcc-avr binutils-avr gdb-avr avr-libc avrdude python3 python3-pip
 echo "Installing python dependencies"
-sudo pip3 install -r requirements.txt
+sudo python3 -m pip install -r requirements.txt
 echo "Writing directory shortcuts"
