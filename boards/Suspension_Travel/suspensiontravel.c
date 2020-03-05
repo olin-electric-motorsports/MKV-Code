f86b77ae3afe078ab794d3d3b1cdb30ca96fdab9
Andrew Mascillaro <amascillaro@gmail.com>
2020-03-05 10:06:38 -0500

    Add timer and basic CAN scripts to Suspension Travel

--- boards/Suspension_Travel/suspensiontravel.c
+++ boards/Suspension_Travel/suspensiontravel.c
@@ -1,5 +1,29 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include "can_api.h"
+
+// Assign names to the three on-board LEDs
+#define LED1 PB5
+#define LED2 PB6
+#define LED3 PB7
+
+/*----- gFlag -----*/
+#define UPDATE_STATUS  	0
+
+/*--- CAN Message ---*/
+#define MSG_INDEX_ERROR_CODE      	0
+#define MSG_INDEX_POTSENSE_HIGHBYTE	1
+#define MSG_INDEX_POTSENSE_LOWBYTE  2
+
+// Linear potentiometer linked to suspension travel distance
+// More information at the confluence page below
+// Including a calibration curve and potentiometer specs
+// https://docs.olinelectricmotorsports.com/display/MD/Suspension+Travel
+#define POTENTIOMETER_INPUT 8
+
+
+uint8_t msg[] = {0,0,0};
+volatile uint8_t gFlag = 0;
+
 
 void ADC_init(void) {
     /* Get the Analog to Digital Converter started (ADC)
@@ -14,10 +38,33 @@ void ADC_init(void) {
     ADMUX |= _BV(REFS0);
 }
 
+void initTimer0(void) {
+    TCCR0A = _BV(WGM01);    // Set up 8-bit timer in CTC mode
+    TCCR0B |= _BV(CS00) | _BV(CS02);          // clkio/1024 prescaler
+    TIMSK0 |= _BV(OCIE0A);  // Every 1024 cycles
+    OCR0A = 65;  // until 65, 65, which then calls for
+                            // the TIMER0_COMPA_vect interrupt
+			    // currently running at 60Hz
+    // ATMEGA runs at 4MHz, the timer count is prescaled by a factor
+    // of 1024, and then the output compare match interrupt is called
+    // every 65 counts to reach 60Hz
+    // 4M / 1024 / 65 = 60Hz
+}
+
+ISR(TIMER0_COMPA_vect) {
+
+		gFlag |= _BV(UPDATE_STATUS);
+		// LEDtimer++;
+		// if(LEDtimer > 100){
+		// 	LEDtimer = 0;
+		// 	LED_PORT ^= _BV(LED2);
+		// }
+}
+
 uint16_t readPotentiometer (void) {
 
 		ADMUX = _BV(REFS0);
-		ADMUX |= 8; // Potentiomenter pin is ADC8
+		ADMUX |= POTENTIOMETER_INPUT; // Potentiomenter pin is ADC8
 		ADCSRA |= _BV(ADSC);
 		loop_until_bit_is_clear(ADCSRA, ADSC);
 		uint16_t val = ADC;
@@ -34,30 +81,36 @@ uint16_t readPotentiometer (void) {
 
 void sendCANMessage (uint8_t msg[]) {
 
-  CAN_transmit(MOB_BROADCAST_CRITICAL,
-								CAN_ID_AIR_CONTROL_CRITICAL,
-								CAN_LEN_AIR_CONTROL_CRITICAL,
-								msg);
+  CAN_transmit(0, 0, 0, msg);
 }
 
 int main (void) {
+    sei();
     ADC_init();
-    DDRB |= _BV(PB5) | _BV(PB6) | _BV(PB7);
-    // DDRB |= _BV(PB6);
-    // DDRB |= _BV(PB7);
+    initTimer0();
+
+    DDRB |= _BV(LED1) | _BV(LED2) | _BV(LED3);
+    
     uint16_t potVolt = 0;
 
     while(1) {
-        PORTB ^= _BV(PB5);
-        potVolt = readPotentiometer();
+        if(bit_is_set(gFlag, UPDATE_STATUS)) {
+            gFlag &= ~_BV(UPDATE_STATUS); // unset UPDATE_STATUS
+            // PORTB ^= _BV(LED1);
+            potVolt = readPotentiometer();
 
-        if(potVolt > 615) {
-            PORTB |= _BV(PB6);
-        } else {
-            PORTB &= ~_BV(PB6);
+            msg[MSG_INDEX_ERROR_CODE] = 0;
+            msg[MSG_INDEX_POTSENSE_HIGHBYTE] = (potVolt >> 8);
+            msg[MSG_INDEX_POTSENSE_LOWBYTE] = (potVolt & 0xff);
+
+            sendCANMessage(msg);
+
+            if(potVolt > 615) {
+                PORTB |= _BV(LED2);
+            } else {
+                PORTB &= ~_BV(LED2);
+            }
         }
-
-        _delay_ms(500);
     }
 
 
