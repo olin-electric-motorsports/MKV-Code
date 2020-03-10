82ed4f957088f10d593f693863f9a7bcbb9e608c
Luke Nonas-Hunter <39748544+nonas-hunter@users.noreply.github.com>
2020-03-09 22:24:55 -0400

    Large Code Update
    Adding the following features:
    - Understandable Macros
    - Test SPI Communication
    - Test Button Press (Interrupt Operations) commented out

--- boards/Accelerometer_Module/Accelerometer_Module.ino
+++ boards/Accelerometer_Module/Accelerometer_Module.ino
@@ -5,34 +5,87 @@
  * in order to complete modal analysis.
  */
 
-
-
-// TODO: Code everything.
-  // Turn on board & set everything up
-  // Initialize Accelerometer
-  // Wait for button press
-  // Record data
-  // Wait for button press
-  // Stop recording data
-  // Send data to SD card
-
-
-// Libraries
+/****** Libraries ******/
 #include <SPI.h>
 #include <SD.h>
 
+/****** LED Macros ******/
+// Voltage Indicator LED
+#define DDR_VLED DDRC
+#define PORT_VLED PORTC
+#define HIGH_VLED PC2
+#define MED_VLED PC1
+#define LOW_VLED PC0
 
-// Variable Definition
+// Status LED
+#define DDR_SLED DDRD
+#define PORT_SLED PORTD
+#define ON_LED PD5
+#define READY_LED PD6
 
+
+/****** Button Macros ******/
+#define BUTTON PD7
+#define DDR_BUTTON DDRD
+
+
+/****** Accelerometer Macros ******/
+#define DDR_ACC DDRB
+#define PORT_ACC PORTB
+#define ACC_CS PB2
+
+#define READ 1
+#define WRITE 0
+#define MULTIPLE 1
+#define SINGLE 0
+#define RWbit 0 // Location of Read/Write Bit (Accelerometer)
+#define MSbit 0 // Location of Multiple/Single Bit (Accelerometer)
+#define WHOAMI 0x0F // Location if WHO AM I register (Accelerometer)
 
 void setup() {
-  // Initalization
+  unsigned int result = 0;
   
+  DDR_SLED |= _BV(READY_LED);
+  DDR_BUTTON &= ~_BV(BUTTON);
+  DDR_VLED |= _BV(HIGH_VLED);
 
+  // Accelerometer CS Pin
+  DDR_ACC |= _BV(ACC_CS);
+
+  // Accelerometer Datasheet - Max Speed 1MHz
+  // Page 27 - SPI MODE 0 - Output on Falling Edge, Data Capture on Rising Edge
+  SPI.beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
+  PORT_ACC &= ~_BV(ACC_CS);
+  result = SPI.transfer(0b11111111);
+  PORT_ACC |= _BV(ACC_CS);
+  SPI.endTransaction();
+  
+  if (result == 0b00110011) {
+    PORT_VLED |= _BV(HIGH_VLED);
+  }
+  
+//  PCICR |= _BV(PCIE2);
+//  PCMSK2 |= _BV(PCINT23);
+//  sei();
 }
 
-void loop() {
-  // Main Loop (button press, record, button press, send)
+//ISR(PCINT2_vect) {
+//  PORTD ^= _BV(PD6);
+//}
 
+void loop() {
+  unsigned int result = 0;
+  SPI.beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
+  PORT_ACC &= ~_BV(ACC_CS);
+  result = SPI.transfer(0b10001111);
+  SPI.endTransaction();
   
+  if (result == 0b00110011) {
+    PORT_ACC |= _BV(ACC_CS);
+  }
+  
+  if (bit_is_set( PINB, PB1 )) {
+    PORT_SLED ^= _BV(READY_LED);
+    delay(1000);
+  }
 }
