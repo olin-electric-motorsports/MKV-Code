71e7c1c9fa59211d4c7fc8417f4e503b5bcaa7c6
nonas-hunter <lukenonashunter@gmail.com>
2020-04-27 21:06:24 -0400

    begining I2C communication with INA260

--- boards/Accelerometer_Module/Accelerometer_Module.ino
+++ boards/Accelerometer_Module/Accelerometer_Module.ino
@@ -2,7 +2,7 @@
  * Board: Accelerometer Module
  * Version: v1.0
  * Description: The goal of this board is to measure the frequency of vibrations in the chassis
- * in order to complete modal analysis.
+ * in order to perform modal analysis.
  */
 
 /****** Libraries ******/
@@ -47,8 +47,18 @@
 #define MSbit 0 // Location of Multiple/Single Bit (Accelerometer)
 #define WHOAMI 0x0F // Location if WHO AM I register (Accelerometer)
 
+
+/****** I2C Macros ******/
+#define INA260_ADDR 1000000
+#define VOLTAGE_REG 0x02
+
+// Variable Declaration
+File data;
+int xyzData[6];
+
 void setup() {
-  unsigned int result = 0;
+
+  // TODO: Add SD.begin(___)
   
   DDR_SLED |= _BV(READY_LED);
   DDR_BUTTON &= ~_BV(BUTTON);
@@ -61,11 +71,6 @@ void setup() {
   DDR_SPI |= _BV(SCK) | _BV(MOSI);
 
   
-  if (result == 0b00110011) {
-    PORT_VLED |= _BV(HIGH_VLED);
-  }
-
-  
 //  PCICR |= _BV(PCIE2);
 //  PCMSK2 |= _BV(PCINT23);
 //  sei();
@@ -76,22 +81,47 @@ void setup() {
 //}
 
 void loop() {
-  unsigned int result = 0;
   
-  
+  memset(xyzData,0,6); // set xyzData to zeros
+
+  // TODO: Turn READ operation into function
   // Accelerometer Datasheet - Max Speed 1MHz
   // Page 27 - SPI MODE 0 - Output on Falling Edge, Data Capture on Rising Edge
   SPI.beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
   PORT_SPI &= ~_BV(ACC_CS);
-  result = SPI.transfer(0b10001111);
+  xyzData[ 0 ] = SPI.transfer(0b10101000); // OUT_X_L
+  PORT_SPI |= _BV(ACC_CS);
+  PORT_SPI &= ~_BV(ACC_CS);
+  xyzData[ 1 ] = SPI.transfer(0b10101001); // OUT_X_H
+  PORT_SPI |= _BV(ACC_CS);
+  PORT_SPI &= ~_BV(ACC_CS);
+  xyzData[ 2 ] = SPI.transfer(0b10101010); // OUT_Y_L
+  PORT_SPI |= _BV(ACC_CS);
+  PORT_SPI &= ~_BV(ACC_CS);
+  xyzData[ 3 ] = SPI.transfer(0b10101011); // OUT_Y_H
+  PORT_SPI |= _BV(ACC_CS);
+  PORT_SPI &= ~_BV(ACC_CS);
+  xyzData[ 4 ] = SPI.transfer(0b10101100); // OUT_Z_L
+  PORT_SPI |= _BV(ACC_CS);
+  PORT_SPI &= ~_BV(ACC_CS);
+  xyzData[ 5 ] = SPI.transfer(0b10101101); // OUT_Z_H
+  PORT_SPI |= _BV(ACC_CS);
   SPI.endTransaction();
-  
 
   
-  
-  if (result == 0b00110011) {
-    PORT_SPI |= _BV(ACC_CS);
+  // creates/opens file "data.txt"
+  // starts writing at END of file
+  data = SD.open("data.txt", FILE_WRITE);
+  for ( int i = 0; i < 3; i++ ) {
+    data.println(xyzData[ i*2 ] >> 6 | xyzData[ (i*2)+1 ] << 2);
   }
+  data.close();
+  
+
+  // I2C Slave Address (INA260) -- 1000000
+  // Bus Voltage register (0x03)
+  // Alert Limit register (0x07)
+ 
   
   if (bit_is_set( PINB, PB1 )) {
     PORT_SLED ^= _BV(READY_LED);
